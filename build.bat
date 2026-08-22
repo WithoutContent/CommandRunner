2ddb05148b83e5c562e1f958bb111d1384d2cd6d
withoutContent <withoutcontent.bezkontentu@gmail.com>
2026-08-22 20:14:43 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,14 @@
-python -m PyInstaller --clean --onedir --noconfirm --noconsole -n "CommandRunner" -i assets\icon.ico src\main.py
+@echo off
+echo Select Pyinstaller option
+echo [1] onedir
+echo [2] onefile
+set /p input=" "
+
+if "%input%"=="1" (
+	python -m PyInstaller --clean --onedir --noconfirm --noconsole -n "CommandRunner" -i assets\icon.ico src\main.py
+) else if "%input%"=="2" (
+	python -m PyInstaller --clean --onefile --noconfirm --noconsole -n "CommandRunner" -i assets\icon.ico src\main.py
+) else exit /b
+
 xcopy assets "dist\CommandRunner" /E /I /Y
 pause
